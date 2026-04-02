3d0096749ae5764684e4c6fd08d98c518b836a7f
Rafael Teles Espindola <rafatespindola@gmail.com>
2026-04-01 21:12:52 -0300

    Limpeza da branch para iniciar do completo zero

--- src/main/java/br/com/fiap/restaurant_platform_api/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.restaurant_platform_api.repository;
-
-import br.com.fiap.restaurant_platform_api.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
